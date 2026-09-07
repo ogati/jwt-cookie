c892670e0ff286a0a83e2cb5ef420980eaf9099f
leija <leija@Lei>
2026-09-06 20:22:20 -0400

    Refactored

--- src/main/java/ca/gc/aafc/employee/api/config/SecurityConfig.java
+++ src/main/java/ca/gc/aafc/employee/api/config/SecurityConfig.java
@@ -39,21 +39,23 @@ public class SecurityConfig {
     
 	@Bean
     SecurityFilterChain filterChain(HttpSecurity http, JwtFilter jwtFilter, Environment env) throws Exception {
-		List<String> urls = new ArrayList<>(List.of("/index.html", "/login"));
+		List<String> publicUrls = new ArrayList<>(List.of("/index.html", "/login"));
+		List<String> csrfIgnoredUrls = new ArrayList<>(List.of("/login"));
 		
 	    if (env.matchesProfiles("dev")) {
-	    	urls.add("/h2-console/**");
+	    	publicUrls.add("/h2-console/**");
+	    	csrfIgnoredUrls.add("/h2-console/**");
     		http.headers(headers -> headers.frameOptions(frame -> frame.disable()));
 	    }
 	    
 		http.csrf(csrf -> 
 					csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
 //						.csrfTokenRequestHandler(new SpaCsrfTokenRequestHandler()) // needed only for SPA frontends
-						.ignoringRequestMatchers("/login", "/h2-console/**"))
+						.ignoringRequestMatchers(csrfIgnoredUrls.toArray(String[]::new)))
     		.sessionManagement(session -> 
 					session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
     		.authorizeHttpRequests(auth -> 
-		  			auth.requestMatchers(urls.toArray(String[]::new)).permitAll()
+		  			auth.requestMatchers(publicUrls.toArray(String[]::new)).permitAll()
 		  				.requestMatchers("/admin/**").hasRole("ADMIN")
 		  				.anyRequest().authenticated())
     		.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
